c9dfac64beb1e134523f520ee63d0536710f896c
Hephzibah <hephzibahijeje89@gmail.com>
2023-08-05 15:20:41 -0700

    safe_print_list.py

---
+++ python_world/101-python_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""writing a Magic Class"""
+import math
+
+
+class MagicClass;
+"""Define a magic class with similar behaviour to the given bytecode."""
+def __init__(self, radius=0);
+"""Initalize the main magic class instance.
+
+Args:
+radius (int or float); The radius of the circle. Default is 0
+"""
+def safe_print_list(my_list = [], x=0);
+count = 0
+total_list = 0
+return (total_list)
---
+++ python_world/safe_print_list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+print.course = "python is for beginners"
